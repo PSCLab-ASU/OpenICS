a62ed7d99e1f0ca14bdf4641f6051efd702e6a3f
Avi <pireads@gmail.com>
2020-02-23 10:50:07 -0700

    got rid of measurement generator because it felt like unessecary overhead, started work on ldamp function

--- LDAMP/ldamp.py
+++ LDAMP/ldamp.py
@@ -3,26 +3,7 @@ import torch
 from torch import Tensor
 
 
-def generate_measurement_operators(mode, m, n):
-    if mode == "gaussian":
-        """values that parameterize the measurement model. 
-        This could be the measurement matrix itself or the 
-        random mask with coded diffraction patterns."""
-        A_val = np.float32(1.0 / np.sqrt(m) * np.random.randn(m, n))
-
-        def A_handle(A_vals, x):
-            return torch.mm(A_vals, x)
-
-        def At_handle(A_vals: Tensor, z):
-            return torch.mm(torch.t(A_vals), z)
-
-        return [A_handle, At_handle, A_val]
-
-    else:
-        raise ValueError("Measurement mode not recognized")
-
-
-def train_ldamp(
+def ldamp(
     sensing="gaussian",
     reconstruction="ldamp",
     stage="training",
@@ -33,6 +14,16 @@ def train_ldamp(
     input_height=128,
     m=10,
     n=100,
-    **kwargs
+    specifics=None,
 ):
-    pass
+    if specifics is None:
+        specifics = {"max_n_DAMP_layers": 10, "start_layer": 1}
+    if stage == "training":
+        for n_DAMP_layers in range(
+            specifics["start_layer"], specifics["max_n_DAMP_layers"] + 1
+        ):
+            n_layers_trained = n_DAMP_layers
+            theta = []
+
+        # A = np.float32(1.0 / np.sqrt(m) * np.random.randn(m, n))
+        # y = torch.mm(A, )
