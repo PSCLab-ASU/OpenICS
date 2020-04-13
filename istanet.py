cde95f4cd98a969cb4806cc139a880abb69d1ca3
Avi <pireads@gmail.com>
2020-04-13 09:51:34 -0600

    testing

--- istanet.py
+++ istanet.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import init
-# import torchvision
 
 # Define ISTA-Net Block
 class BasicBlock(torch.nn.Module):
--- reconstruction_methods.py
+++ reconstruction_methods.py
@@ -2,7 +2,12 @@ import numpy as np
 import scipy.io as sio
 import torch
 from torch import nn
+import cv2
+from time import time
+import glob
 from istanet import ISTANetModel
+from skimage.measure import compare_ssim as ssim
+from utils import rgb2ycbcr, ycbcr2rgb, imread_CS_py, img2col_py, col2im_CS_py, psnr
 
 
 def reconstruction_method(reconstruction, specifics):
@@ -81,7 +86,9 @@ class ISTANet:
                     #     "PHI SIZE AFTER MULT:",
                     #     self.Phi.size(),
                     # )
-                    [x_output, loss_layers_sym] = self.model.forward(Phix, self.Phi, self.Qinit)
+                    [x_output, loss_layers_sym] = self.model.forward(
+                        Phix, self.Phi, self.Qinit
+                    )
 
                     # Compute and print loss
                     loss_discrepancy = torch.mean(torch.pow(x_output - batch_x, 2))
@@ -118,3 +125,109 @@ class ISTANet:
                         torch.save(
                             self.model.state_dict(), f"models/net_params_{epoch}.pkl"
                         )  # save only the parameters
+        elif stage == "testing":
+            self.model.load_state_dict(
+                torch.load(
+                    f"models/istanet_params_{self.specifics['epoch_number']}.pkl"
+                )
+            )
+
+            print("CS Reconstruction Start")
+            filepaths = glob.glob(f"data/{self.dataset}/*.tif")
+
+            result_dir = "results"
+
+            ImgNum = len(filepaths)
+            PSNR_All = np.zeros([1, ImgNum], dtype=np.float32)
+            SSIM_All = np.zeros([1, ImgNum], dtype=np.float32)
+
+            with torch.no_grad():
+                for img_no in range(self.specifics["test_image_number"]):
+                    imgName = filepaths[img_no]
+
+                    Img = cv2.imread(imgName, 1)
+
+                    Img_yuv = cv2.cvtColor(Img, cv2.COLOR_BGR2YCrCb)
+                    Img_rec_yuv = Img_yuv.copy()
+
+                    Iorg_y = Img_yuv[:, :, 0]
+
+                    [Iorg, row, col, Ipad, row_new, col_new] = imread_CS_py(Iorg_y)
+                    Icol = img2col_py(Ipad, 33).transpose() / 255.0
+
+                    Img_output = Icol
+
+                    start = time()
+
+                    batch_x = torch.from_numpy(Img_output)
+                    batch_x = batch_x.type(torch.FloatTensor)
+                    batch_x = batch_x.to(self.device)
+
+                    Phix = torch.mm(batch_x, torch.transpose(self.Phi, 0, 1))
+
+                    [x_output, loss_layers_sym] = self.model(Phix, self.Phi, self.Qinit)
+
+                    end = time()
+
+                    Prediction_value = x_output.cpu().data.numpy()
+
+                    # loss_sym = torch.mean(torch.pow(loss_layers_sym[0], 2))
+                    # for k in range(layer_num - 1):
+                    #     loss_sym += torch.mean(torch.pow(loss_layers_sym[k + 1], 2))
+                    #
+                    # loss_sym = loss_sym.cpu().data.numpy()
+
+                    X_rec = np.clip(
+                        col2im_CS_py(
+                            Prediction_value.transpose(), row, col, row_new, col_new
+                        ),
+                        0,
+                        1,
+                    )
+
+                    rec_PSNR = psnr(X_rec * 255, Iorg.astype(np.float64))
+                    rec_SSIM = ssim(
+                        X_rec * 255, Iorg.astype(np.float64), data_range=255
+                    )
+
+                    print(
+                        "[%02d/%02d] Run time for %s is %.4f, PSNR is %.2f, SSIM is %.4f"
+                        % (img_no, ImgNum, imgName, (end - start), rec_PSNR, rec_SSIM)
+                    )
+
+                    Img_rec_yuv[:, :, 0] = X_rec * 255
+
+                    im_rec_rgb = cv2.cvtColor(Img_rec_yuv, cv2.COLOR_YCrCb2BGR)
+                    im_rec_rgb = np.clip(im_rec_rgb, 0, 255).astype(np.uint8)
+
+                    resultName = imgName.replace("data", "output")
+                    cv2.imwrite(
+                        "%s_ISTA_Net_ratio_%d_epoch_%d_PSNR_%.2f_SSIM_%.4f.png"
+                        % (
+                            resultName,
+                            self.specifics["cs_ratio"],
+                            self.specifics["epoch_num"],
+                            rec_PSNR,
+                            rec_SSIM,
+                        ),
+                        im_rec_rgb,
+                    )
+                    del x_output
+
+                    PSNR_All[0, img_no] = rec_PSNR
+                    SSIM_All[0, img_no] = rec_SSIM
+
+            print("\n")
+            output_data = (
+                "CS ratio is %d, Avg PSNR/SSIM for %s is %.2f/%.4f, Epoch number of model is %d \n"
+                % (
+                    self.specifics["cs_ratio"],
+                    self.dataset,
+                    np.mean(PSNR_All),
+                    np.mean(SSIM_All),
+                    self.specifics["epoch_num"],
+                )
+            )
+            print(output_data)
+
+            print("CS Reconstruction End")
