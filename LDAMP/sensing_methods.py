d59d169e73666a1602af146bd6df8ccfc2b0b5f4
BurningPride <56182492+mwesterham@users.noreply.github.com>
2020-10-05 15:42:08 -0700

    Add files via upload

--- LDAMP/sensing_methods.py
+++ LDAMP/sensing_methods.py
@@ -1,219 +1,62 @@
-#TODO tensorflow version 2.X migration code changed the import tensorflow as tf line to two lines as seen below
-# import tensorflow.compat.v1 as tf
-# tf.disable_eager_execution()
-import tensorflow as tf
+import torch
+from torch import nn
 import numpy as np
-import os
-os.environ["CUDA_VISIBLE_DEVICES"]='1'
-def SetNetworkParams(new_height_img, new_width_img,new_channel_img, new_filter_height,new_filter_width,\
-                     new_num_filters,new_n_DnCNN_layers,new_n_DAMP_layers, new_sampling_rate,\
-                     new_BATCH_SIZE,new_sigma_w,new_n,new_m,new_training,use_adaptive_weights=False):
-    global height_img, width_img, channel_img, filter_height, filter_width, num_filters, n_DnCNN_layers, n_DAMP_layers,\
-        sampling_rate, BATCH_SIZE, sigma_w, n, m, n_fp, m_fp, is_complex, training, adaptive_weights
-    height_img = new_height_img
-    width_img = new_width_img
-    channel_img = new_channel_img
-    filter_height = new_filter_height
-    filter_width = new_filter_width
-    num_filters = new_num_filters
-    n_DnCNN_layers = new_n_DnCNN_layers
-    n_DAMP_layers = new_n_DAMP_layers
-    sampling_rate = new_sampling_rate
-    BATCH_SIZE = new_BATCH_SIZE
-    sigma_w = new_sigma_w
-    n = new_n
-    m = new_m
-    n_fp = np.float32(n)
-    m_fp = np.float32(m)
-    is_complex=False#Just the default
-    adaptive_weights=use_adaptive_weights
-    training=new_training
 
-def sensing_method(method_name,specifics):
-    # a function which returns a sensing method with given parameters. a sensing method is a subclass of nn.Module
-    return 1
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
 
-#Form the measurement operators
-def GenerateMeasurementOperators(mode):
-    global is_complex
-    global sparse_sampling_matrix #TODO moved this to declare before assigning python 2.7 vs 3.7
-    if mode=='gaussian':
-        is_complex=False
-        A_val = np.float32(1. / np.sqrt(m_fp) * np.random.randn(m, n))# values that parameterize the measurement model. This could be the measurement matrix itself or the random mask with coded diffraction patterns.
-        y_measured = tf.placeholder(tf.float32, [m, None])
-        A_val_tf = tf.placeholder(tf.float32, [m, n])  #A placeholer is used so that the large matrix isn't put into the TF graph (2GB limit)
+def sensing_method(sensing,specifics,m,n):
+    sense = random_sensing(specifics)
+    return sense
 
-        def A_handle(A_vals_tf,x):
-            return tf.matmul(A_vals_tf,x)
+class random_sensing(nn.Module):
+    def __init__(self, specifics):
+        super(random_sensing, self).__init__()
+        self.n = specifics["n"]
+        self.m = specifics["m"]
+        self.sigma = specifics["sigma_w"]
+        self.s1 = nn.Linear(self.n, self.m, bias=False)
+        self.from_Numpy_matrix = 0
+        self.sm = 0
 
-        def At_handle(A_vals_tf,z):
-            return tf.matmul(A_vals_tf,z,adjoint_a=True)
+        # initial sensing matrix
+        self.from_Numpy_matrix = np.float32(1.0 / np.sqrt(self.m) * np.random.randn(self.m, self.n))
+        self.sm = torch.from_numpy(self.from_Numpy_matrix).to(device)
+        self.s1.weight.data = self.sm
 
-    elif mode == 'complex-gaussian':
-            is_complex = True
-            A_val = np.complex64(1/np.sqrt(2.)*((1. / np.sqrt(m_fp) * np.random.randn(m,n))+1j*(1. / np.sqrt(m_fp) * np.random.randn(m,n))))
-            y_measured = tf.placeholder(tf.complex64, [m, None])
-            A_val_tf = tf.placeholder(tf.complex64, [m, n])  # A placeholer is used so that the large matrix isn't put into the TF graph (2GB limit)
+    def forward(self, x):
+        r = self.s1(x)
+        noise = self.sigma * torch.rand(r.shape).to(device)
+        r = r + noise
+        return r
 
-            def A_handle(A_vals_tf, x):
-                return tf.matmul(A_vals_tf, tf.complex(x,tf.zeros([n,BATCH_SIZE],dtype=tf.float32)))
+    def generateNewMatrix(self):
+        self.from_Numpy_matrix = np.float32(1.0 / np.sqrt(self.m) * np.random.randn(self.m, self.n))
+        self.sm = torch.from_numpy(self.from_Numpy_matrix).to(device)
+        self.s1.weight.data = self.sm
 
-            def At_handle(A_vals_tf, z):
-                return tf.matmul(A_vals_tf, z, adjoint_a=True)
-    elif mode=='coded-diffraction':
-        is_complex=True
-        A_val = np.zeros([n, 1]) + 1j * np.zeros([n, 1])
-        A_val[0:n] = np.exp(1j*2*np.pi*np.random.rand(n,1))#The random sign vector
+    def returnSensingMatrix(self):
+        return self.sm
 
-        #global sparse_sampling_matrix
-        rand_col_inds=np.random.permutation(range(n))
-        rand_col_inds=rand_col_inds[0:m]
-        row_inds = range(m)
-        inds=zip(row_inds,rand_col_inds)
-        vals=tf.ones(m, dtype=tf.complex64);
-        sparse_sampling_matrix = tf.SparseTensor(indices=inds, values=vals, dense_shape=[m,n])
+    def setSensingMatrix(self, A_val):
+        self.from_Numpy_matrix = A_val
+        self.sm = torch.from_numpy(self.from_Numpy_matrix).to(device)
+        self.s1.weight.data = self.sm
 
-        A_val_tf = tf.placeholder(tf.complex64, [n, 1])
-        def A_handle(A_val_tf, x):
-            sign_vec = A_val_tf[0:n]
-            signed_x = tf.multiply(sign_vec, tf.complex(x,tf.zeros([n,BATCH_SIZE],dtype=tf.float32)))
-            signed_x = tf.reshape(signed_x, [height_img, width_img, BATCH_SIZE])
-            signed_x=tf.transpose(signed_x)#Transpose because fft2d operates upon the last two axes
-            F_signed_x = tf.fft2d(signed_x)
-            F_signed_x=tf.transpose(F_signed_x)
-            F_signed_x = tf.reshape(F_signed_x, [height_img * width_img, BATCH_SIZE])*1./np.sqrt(m_fp)#This is a different normalization than in Matlab because the FFT is implemented differently in Matlab
-            out = tf.sparse_tensor_dense_matmul(sparse_sampling_matrix,F_signed_x,adjoint_a=False)
-            return out
+class original_sensing():
+    def __init__(self, specifics, from_Numpy_matrix):
+        self.sigma_w = specifics["sigma_w"]
+        self.A_val = from_Numpy_matrix
 
-        def At_handle(A_val_tf, z):
-            sign_vec=A_val_tf[0:n]
-            z_padded = tf.sparse_tensor_dense_matmul(sparse_sampling_matrix,z,adjoint_a=True)
-            z_padded = tf.reshape(z_padded, [height_img, width_img, BATCH_SIZE])
-            z_padded=tf.transpose(z_padded)#Transpose because fft2d operates upon the last two axes
-            Finv_z = tf.ifft2d(z_padded)
-            Finv_z = tf.transpose(Finv_z)
-            Finv_z = tf.reshape(Finv_z, [height_img*width_img, BATCH_SIZE])
-            out = tf.multiply(tf.conj(sign_vec), Finv_z)*n_fp/np.sqrt(m)
-            return out
-    elif mode=='Fast-JL':#Measurement matrix close to a fast JL transform. True fast JL would use hadamard transform and a sparse sampling matrix with multiple nz elements per row
-        is_complex=False
-        A_val = np.zeros([n, 1])
-        A_val[0:n] = np.sign(2*np.random.rand(n,1)-1)
+    def sense(self, x):
+        y = torch.matmul(torch.Tensor(self.A_val).to(device), torch.t(x.type(torch.FloatTensor).to(device)))
+        clean = torch.tensor(y)
+        noise_vec = torch.rand(clean.shape)
+        noise_vec = self.sigma_w * np.reshape(noise_vec, newshape=clean.shape)
+        noisy_data = clean + noise_vec.to(device)
+        return noisy_data
 
-        #global sparse_sampling_matrix
-        rand_col_inds=np.random.permutation(range(n))
-        rand_col_inds=rand_col_inds[0:m]
-        row_inds = range(m)
-        inds=zip(row_inds,rand_col_inds)
-        vals=tf.ones(m, dtype=tf.float32);
-        sparse_sampling_matrix = tf.SparseTensor(indices=inds, values=vals, dense_shape=[m,n])
-
-        A_val_tf = tf.placeholder(tf.float32, [n, 1])
-        def A_handle(A_val_tf, x):
-            sign_vec = A_val_tf[0:n]
-            signed_x = tf.multiply(sign_vec, x)
-            signed_x = tf.reshape(signed_x, [height_img*width_img, BATCH_SIZE])
-            signed_x=tf.transpose(signed_x)#Transpose because dct operates upon the last axes
-            F_signed_x = mydct(signed_x, type=2, norm='ortho')
-            F_signed_x=tf.transpose(F_signed_x)
-            F_signed_x = tf.reshape(F_signed_x, [height_img * width_img, BATCH_SIZE])*np.sqrt(n_fp/m_fp)
-            out = tf.sparse_tensor_dense_matmul(sparse_sampling_matrix,F_signed_x,adjoint_a=False)
-            return out
-
-        def At_handle(A_val_tf, z):
-            sign_vec=A_val_tf[0:n]
-            z_padded = tf.sparse_tensor_dense_matmul(sparse_sampling_matrix,z,adjoint_a=True)
-            z_padded = tf.reshape(z_padded, [height_img*width_img, BATCH_SIZE])
-            z_padded=tf.transpose(z_padded)#Transpose because dct operates upon the last axes
-            Finv_z = myidct(z_padded,type=2,norm='ortho')
-            Finv_z = tf.transpose(Finv_z)
-            Finv_z = tf.reshape(Finv_z, [height_img*width_img, BATCH_SIZE])
-            out = tf.multiply(sign_vec, Finv_z)*np.sqrt(n_fp/m_fp)
-            return out
-    else:
-        raise ValueError('Measurement mode not recognized')
-    return [A_handle, At_handle, A_val, A_val_tf]
-
-def GenerateMeasurementMatrix(mode):
-    if mode == 'gaussian':
-        A_val = np.float32(1. / np.sqrt(m_fp) * np.random.randn(m,n))  # values that parameterize the measurement model. This could be the measurement matrix itself or the random mask with coded diffraction patterns.
-    elif mode=='complex-gaussian':
-        A_val = np.complex64(1 / np.sqrt(2.) * ((1. / np.sqrt(m_fp) * np.random.randn(m, n)) + 1j * (1. / np.sqrt(m_fp) * np.random.randn(m, n))))
-    elif mode=='coded-diffraction':
-        A_val = np.zeros([n, 1]) + 1j * np.zeros([n, 1])
-        A_val[0:n] = np.exp(1j*2*np.pi*np.random.rand(n,1))#The random sign vector
-        rand_col_inds=np.random.permutation(range(n))
-        rand_col_inds=rand_col_inds[0:m]
-        row_inds = range(m)
-        inds=zip(row_inds,rand_col_inds)
-        vals=tf.ones(m, dtype=tf.complex64);
-        sparse_sampling_matrix = tf.SparseTensor(indices=inds, values=vals, dense_shape=[m,n])
-    elif mode == 'Fast-JL':
-        A_val = np.zeros([n, 1])
-        A_val[0:n] = np.sign(2*np.random.rand(n,1)-1)
-        rand_col_inds=np.random.permutation(range(n))
-        rand_col_inds=rand_col_inds[0:m]
-        row_inds = range(m)
-        inds=zip(row_inds,rand_col_inds)
-        vals=tf.ones(m, dtype=tf.float32);
-        sparse_sampling_matrix = tf.SparseTensor(indices=inds, values=vals, dense_shape=[m,n])
-    else:
-        raise ValueError('Measurement mode not recognized')
-    return A_val
-
-def mydct(x,type=2,norm='ortho'):
-    assert type==2 and norm=='ortho', 'Currently only type-II orthonormalized DCTs are supported'
-    assert BATCH_SIZE == 1, 'Fast-JL measurement matrices currently only support batch sizes of one'
-    #https://antimatter15.com/2015/05/cooley-tukey-fft-dct-idct-in-under-1k-of-javascript/
-    y=tf.concat([x,tf.zeros([1,n],tf.float32)],axis=1)
-    Y=tf.fft(tf.complex(y,tf.zeros([1,2*n],tf.float32)))
-    Y=Y[:,:n]
-    k = tf.complex(tf.range(n, dtype=tf.float32), tf.zeros(n, dtype=tf.float32))
-    Y*=tf.exp(-1j*np.pi*k/(2.*n_fp))
-    return tf.real(Y)/tf.sqrt(n_fp)*tf.sqrt(2.)
-    # return tf.spectral.dct(x,type=2,norm='ortho')
-def myidct(X,type=2,norm='ortho'):
-    assert type==2 and norm=='ortho', 'Currently only type-II orthonormalized DCTs are supported'
-    assert BATCH_SIZE==1, 'Fast-JL measurement matrices currently only support batch sizes of one'
-    #https://antimatter15.com/2015/05/cooley-tukey-fft-dct-idct-in-under-1k-of-javascript/
-    temp0=tf.reverse(X,[-1])
-    temp1=tf.manip.roll(temp0,shift=1,axis=1)
-    temp2=temp1[:,1:]
-    temp3=tf.pad(temp2,[[0,0],[1,0]],"CONSTANT")
-    Z=tf.complex(X,-temp3)
-    k = tf.complex(tf.range(n,dtype=tf.float32),tf.zeros(n,dtype=tf.float32))
-    Z*=tf.exp(1j*np.pi*k/(2.*n_fp))
-    temp4=tf.real(tf.ifft(Z))
-    even_new=temp4[:,0:n/2]
-    odd_new=tf.reverse(temp4[:,n/2:],[-1])
-    #https://stackoverflow.com/questions/44952886/tensorflow-merge-two-2-d-tensors-according-to-even-and-odd-indices
-    x=tf.reshape(
-        tf.transpose(tf.concat([even_new, odd_new], axis=0)),
-        [1,n])
-    return tf.real(x)*tf.sqrt(n_fp)*1/tf.sqrt(2.)
-def mydct_np(x,type=2,norm='ortho'):
-    assert type==2 and norm=='ortho', 'Currently only type-II orthonormalized DCTs are supported'
-    assert BATCH_SIZE == 1, 'Fast-JL measurement matrices currently only support batch sizes of one'
-    #https://antimatter15.com/2015/05/cooley-tukey-fft-dct-idct-in-under-1k-of-javascript/
-    N=len(x)
-    y=np.zeros(2*N)
-    y[:N]=x
-    Y=np.fft.fft(y)[:N]
-    k=np.float32(range(N))
-    Y*=np.exp(-1j*np.pi*k/(2*N))/np.sqrt(N/2.)
-    return Y.real
-def myidct_np(X,type=2,norm='ortho'):
-    assert type==2 and norm=='ortho', 'Currently only type-II orthonormalized DCTs are supported'
-    assert BATCH_SIZE == 1, 'Fast-JL measurement matrices currently only support batch sizes of one'
-    #https://antimatter15.com/2015/05/cooley-tukey-fft-dct-idct-in-under-1k-of-javascript/
-    N=len(X)
-    Z=X-1j*np.append([0.],np.flip(X,0)[:N-1])
-    k = np.float32(range(N))
-    Z*=np.exp(1j*np.pi*k/(2*N))
-    temp=np.real(np.fft.ifft(Z))
-    x=np.zeros(X.size)
-    even_new= temp[0:N/2]
-    odd_new=np.flip(temp[N/2:],0)
-    x[0::2] =even_new
-    x[1::2]=odd_new
-    return np.real(x)*np.sqrt(N/2.)
+    def returnSensingMatrix(self):
+        return torch.Tensor(self.A_val)
--- LDAMP/utils.py
+++ LDAMP/utils.py
@@ -1,155 +1,105 @@
-#TODO tensorflow version 2.X migration code changed the import tensorflow as tf line to two lines as seen below
-# import tensorflow.compat.v1 as tf
-# tf.disable_eager_execution()
-import tensorflow as tf
+import skimage.measure as skmeasure
+import torch
 import numpy as np
-import os
-os.environ["CUDA_VISIBLE_DEVICES"]='1'
-def SetNetworkParams(new_height_img, new_width_img,new_channel_img, new_filter_height,new_filter_width,\
-                     new_num_filters,new_n_DnCNN_layers,new_n_DAMP_layers, new_sampling_rate,\
-                     new_BATCH_SIZE,new_sigma_w,new_n,new_m,new_training,use_adaptive_weights=False):
-    global height_img, width_img, channel_img, filter_height, filter_width, num_filters, n_DnCNN_layers, n_DAMP_layers,\
-        sampling_rate, BATCH_SIZE, sigma_w, n, m, n_fp, m_fp, is_complex, training, adaptive_weights
-    height_img = new_height_img
-    width_img = new_width_img
-    channel_img = new_channel_img
-    filter_height = new_filter_height
-    filter_width = new_filter_width
-    num_filters = new_num_filters
-    n_DnCNN_layers = new_n_DnCNN_layers
-    n_DAMP_layers = new_n_DAMP_layers
-    sampling_rate = new_sampling_rate
-    BATCH_SIZE = new_BATCH_SIZE
-    sigma_w = new_sigma_w
-    n = new_n
-    m = new_m
-    n_fp = np.float32(n)
-    m_fp = np.float32(m)
-    is_complex=False#Just the default
-    adaptive_weights=use_adaptive_weights
-    training=new_training
+from torch.utils.data import Dataset
+import scipy.linalg as la
 
-def generate_dataset(dataset,input_channel,input_width,input_height,stage):
-    # a function to generate the corresponding dataset with given parameters. return an instance of the dataset class.
-    return 1
 
-## Evaluate Intermediate Error
+class NumpyDataset(Dataset):
+    """Dataset provided by the github repository"""
+    def __init__(self, dataset, transform=None):
+        self.dset = dataset
+        self.transform = transform
+        if(self.transform):
+            self.dset = self.transform(torch.Tensor(self.dset), self.dimension)
+        self.dimension = 2
+
+    def __len__(self):
+        return len(self.dset)
+
+    def __getitem__(self, idx):
+        return self.dset[idx]
+
+def generate_dataset(dataset,input_channel,input_width,input_height,stage,specifics):
+    resume = specifics["resume"]
+
+    numpyDataset = np.load(dataset)
+    if(stage=='testing'):
+        n_val_images = specifics["n_val_images"]
+        numpyDataset = numpyDataset[:n_val_images, :, :, :].astype(float)
+    elif(resume):
+        n_train_images = specifics['n_train_images']
+        numpyDataset = numpyDataset[:n_train_images, :, :, :].astype(float)
+    else:
+        n_train_images = specifics['n_train_images']
+        numpyDataset = numpyDataset[:n_train_images, :, :, :].astype(float)
+
+    numpyDataset = np.reshape(numpyDataset, (-1, input_channel * input_width * input_height))
+    torchDataset = NumpyDataset(numpyDataset)
+    return torchDataset
+
+def A_handle(A_val, x):
+    r = torch.matmul(A_val, x)
+    return r
+
+def At_handle(A_val, z): # A^H * z
+    # A_val = A_val.cpu()
+    # A_val = A_val.detach().numpy()
+    # A_val = A_val.T.conj()#torch.t(A_val) # for an all-real matrix A.T.conj() is the same as A.T
+    # A_val = torch.Tensor(A_val).cuda()
+    # r = torch.matmul(A_val, z)
+
+    # TODO SIGNIFICANT CHANGE HERE
+    # original backward operator was conj(A_val).transpose
+    # np.ortho => opitimal sudo-inverse is the transpose of the np.orth0 matrix
+    A_val_inv = la.pinv(A_val.cpu()) # scipy.linalg.pinv()
+    A_val_inv = torch.tensor(A_val_inv)
+    r = torch.matmul(A_val_inv, z)
+    return r
+
 def EvalError(x_hat,x_true):
-    mse=tf.reduce_mean(tf.square(x_hat-x_true),axis=0)
-    xnorm2=tf.reduce_mean(tf.square( x_true),axis=0)
-    mse_thisiter=mse
-    nmse_thisiter=mse/xnorm2
-    psnr_thisiter=10.*tf.log(1./mse)/tf.log(10.)
-    return mse_thisiter, nmse_thisiter, psnr_thisiter
+    mse=((x_hat-x_true)**2).mean()
+    psnrs = [skmeasure.compare_psnr(x_hat[i], x_true[i], data_range=1.0) for i in range(x_hat.shape[0])]
+    psnr = sum(psnrs) / len(psnrs)
 
-## Evaluate Intermediate Error
-def EvalError_np(x_hat,x_true):
-    mse=np.mean(np.square(x_hat-x_true),axis=0)
-    xnorm2=np.mean(np.square( x_true),axis=0)
-    mse_thisiter=mse
-    nmse_thisiter=mse/xnorm2
-    psnr_thisiter=10.*np.log(1./mse)/np.log(10.)
-    return mse_thisiter, nmse_thisiter, psnr_thisiter
+    return mse, psnr
 
-## Output Denoiser Gaussian
-def g_out_gaussian(phat,pvar,y,wvar):
-    g=(y-phat)*1/(pvar+wvar)
-    dg=-1/(pvar+wvar)
-    return g, dg
 
-## Output Denoiser Rician
-def g_out_phaseless(phat,pvar,y,wvar):
-    #Untested. To be used with D-prGAMP
+"""
+Code for Debugging
+"""
+# for i in range(len(dset)):
+#     sample = dset[i]
+#     sample = (np.reshape(sample, (specifics["input_width"], specifics["input_height"], specifics["input_channel"])))
+#     # sample is from 0 to 1 already
+#     # print(i, sample[:,:,0])
+#     plt.imshow(sample)#, cmap='gray')
+#     plt.show()
+#     if (i == 2):
+#         break;
 
-    y_abs = y
-    phat_abs = tf.abs(phat)
-    B = 2.* tf.div(tf.multiply(y_abs,phat_abs),wvar+pvar)
-    I1overI0 = tf.minimum(tf.div(B,tf.sqrt(tf.square(B)+4)),tf.div(B,0.5+tf.sqrt(tf.square(B)+0.25)))
-    y_sca = tf.div(y_abs,1.+tf.div(wvar,pvar))
-    phat_sca = tf.div(phat_abs,1.+tf.div(pvar,wvar))
-    zhat = tf.multiply(tf.add(phat_sca,tf.multiply(y_sca,I1overI0)),tf.sign(phat))
+# if channel == 1:
+# if channel ==3:
+#     r1 = torch.matmul(A_vals[0, :, :], z[0, :, :]) # (m x m)(m x BATCH_SIZE)
+#     r2 = torch.matmul(A_vals[1, :, :], z[1, :, :])
+#     r3 = torch.matmul(A_vals[2, :, :], z[2, :, :])
+#     r = torch.cat([r1, r2, r3])
 
-    sigma2_z = tf.add(tf.add(tf.square(y_sca),tf.square(phat_sca)),tf.subtract(tf.div(1.+tf.multiply(B,I1overI0),tf.add(tf.div(1.,wvar),tf.div(1.,pvar))),tf.square(tf.abs(zhat))))
 
-    g = tf.multiply(tf.div(1.,pvar),tf.subtract(zhat,phat))
-    dg = tf.multiply(tf.div(1.,pvar),tf.subtract(tf.div(tf.reduce_mean(sigma2_z,axis=(1,2,3)),pvar),1))
+# images = glob.glob(dataset + "/*.bmp")
+# dataset = []
+# for img_index, image in enumerate(images):
+#     with open(image, 'rb') as file:
+#         if((img_index + 1) % 1000 == 0):
+#             print("Processed " + str(img_index + 1) + " images")
+#         img = Image.open(file)
+#         np_img = np.array(img).astype(float)
+#         dataset.append(np_img)
+#         if (img_index + 1 == n_train_images):
+#             break;
+# dataset = np.array(dataset)
+# location_and_name = 'Data/BSD500Custom/BSD500-255-FIRST' + str(n_train_images) + ".npy"
+# np.save(location_and_name, dataset)
 
-    return g,dg
-
-## Create training data from images, with tf and function handles
-def GenerateNoisyCSData_handles(x,A_handle,sigma_w,A_params):
-    y = A_handle(A_params,x)
-    y = AddNoise(y,sigma_w)
-    return y
-
-## Create training data from images, with tf
-def GenerateNoisyCSData(x,A,sigma_w):
-    y = tf.matmul(A,x)
-    y = AddNoise(y,sigma_w)
-    return y
-
-## Create training data from images, with tf
-def AddNoise(clean,sigma):
-    if is_complex:
-        noise_vec = sigma/np.sqrt(2) *( tf.complex(tf.random_normal(shape=clean.shape, dtype=tf.float32),tf.random_normal(shape=clean.shape, dtype=tf.float32)))
-    else:
-        noise_vec=sigma*tf.random_normal(shape=clean.shape,dtype=tf.float32)
-    noisy=clean+noise_vec
-    noisy=tf.reshape(noisy,clean.shape)
-    return noisy
-
-## Create training data from images, with numpy
-def GenerateNoisyCSData_np(x,A,sigma_w):
-    y = np.matmul(A,x)
-    y = AddNoise_np(y,sigma_w)
-    return y
-
-## Create training data from images, with numpy
-def AddNoise_np(clean,sigma):
-    noise_vec=np.random.randn(clean.size)
-    noise_vec = sigma * np.reshape(noise_vec, newshape=clean.shape)
-    noisy=clean+noise_vec
-    return noisy
-
-##Create a string that generates filenames. Ensures consitency between functions
-def GenLDAMPFilename(alg,tie_weights,LayerbyLayer,n_DAMP_layer_override=None,sampling_rate_override=None,loss_func='MSE'):
-    if n_DAMP_layer_override:
-        n_DAMP_layers_save=n_DAMP_layer_override
-    else:
-        n_DAMP_layers_save=n_DAMP_layers
-    if sampling_rate_override:
-        sampling_rate_save=sampling_rate_override
-    else:
-        sampling_rate_save=sampling_rate
-    if loss_func=='SURE':
-        filename = "./saved_models/LDAMP/SURE_"+alg+"_" + str(n_DnCNN_layers) + "DnCNNL_" + str(int(n_DAMP_layers_save)) + "DAMPL_Tie"+str(tie_weights)+"_LbyL"+str(LayerbyLayer)+"_SR" +str(int(sampling_rate_save*100))
-    elif loss_func=='GSURE':
-        filename = "./saved_models/LDAMP/GSURE_"+alg+"_" + str(n_DnCNN_layers) + "DnCNNL_" + str(int(n_DAMP_layers_save)) + "DAMPL_Tie"+str(tie_weights)+"_LbyL"+str(LayerbyLayer)+"_SR" +str(int(sampling_rate_save*100))
-    else:
-        filename = "./saved_models/LDAMP/"+alg+"_" + str(n_DnCNN_layers) + "DnCNNL_" + str(int(n_DAMP_layers_save)) + "DAMPL_Tie"+str(tie_weights)+"_LbyL"+str(LayerbyLayer)+"_SR" +str(int(sampling_rate_save*100))
-    return filename
-
-##Create a string that generates filenames. Ensures consitency between functions
-def GenDnCNNFilename(sigma_w_min,sigma_w_max,useSURE=False):
-    if useSURE:
-        filename = "./saved_models/DnCNN/SURE_DnCNN_" + str(n_DnCNN_layers) + "L_sigmaMin" + str(
-            int(255. * sigma_w_min)) + "_sigmaMax" + str(int(255. * sigma_w_max))
-    else:
-        filename="./saved_models/DnCNN/DnCNN_" + str(n_DnCNN_layers) + "L_sigmaMin" + str(int(255.*sigma_w_min))+"_sigmaMax" + str(int(255.*sigma_w_max))
-    return filename
-
-## Calculate Monte Carlo SURE Loss
-def MCSURE_loss(x_hat,div_overN,y,sigma_w):
-    return tf.reduce_sum(tf.reduce_sum((y - x_hat) ** 2, axis=0) / n_fp -  sigma_w ** 2 + 2. * sigma_w ** 2 * div_overN)
-
-## Calculate Monte Carlo Generalized SURE Loss (||Px||^2 term ignored below)
-def MCGSURE_loss(x_hat,x_ML,P,MCdiv,sigma_w):
-    Pxhatnorm2 = tf.reduce_sum(tf.abs(tf.matmul(P, x_hat)) ** 2, axis=0)
-    temp0 = tf.multiply(x_hat, x_ML)
-    x_hatt_xML = tf.reduce_sum(temp0, axis=0)  # x_hat^t*(A^\dagger y)
-    return tf.reduce_sum(Pxhatnorm2+2.*sigma_w**2*MCdiv-2.*x_hatt_xML)
-
-## Calculate Monte Carlo Generalized SURE Loss, ||Px||^2 explicitly added so that estimate tracks MSE
-def MCGSURE_loss_oracle(x_hat,x_ML,P,MCdiv,sigma_w,x_true):
-    Pxtruenorm2 = tf.reduce_sum(tf.abs(tf.matmul(P, x_true)) ** 2, axis=0)
-    return tf.reduce_sum(Pxtruenorm2)+MCGSURE_loss(x_hat,x_ML,P,MCdiv,sigma_w)
+# location_and_name = 'Data/BSD500Custom/BSD500-255-FIRST' + str(n_train_images) + ".npy"
+# location_and_name = 'Data/TrainingData/StandardTestData_256Res.npy'
